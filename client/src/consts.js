0859f84145415e481f2d290c67c343824a678cbd
Matvey Chukhno <mr.chucno@yandex.ru>
2023-05-01 20:45:42 +0300

    Add features

--- client/src/consts.js
+++ client/src/consts.js
@@ -1,9 +1,15 @@
-import MainPage from "./components/pages/MainPage"
+import MainPageGame from "./components/pages/MainPageGame"
+import StatisticPage from "./components/pages/Statistic"
+
+// Host
+export const BASE_URL = "http://127.0.0.1:8000";
 
 // Pathes
 export const MAIN_PATH = "/main"
+export const STATISTIC_PATH = "/statistic"
 
 // Routes
 export const routes = [
-  { path: MAIN_PATH, Component: MainPage}
+  { path: MAIN_PATH, Component: MainPageGame},
+  { path: STATISTIC_PATH, Component: StatisticPage}
 ]
