1bfd22e0fdeec45d6d48de8a2404f116b96e1d48
shk3t <daniildudov@mail.ru>
2023-05-01 21:08:28 +0300

    gamelogic

---
+++ client/src/models/Square.js
@@ -0,0 +1,41 @@
+export default class Square {
+  constructor(index, color, board, piece = null) {
+    this.index = index
+    this.color = color
+    this.piece = piece
+    this.board = board
+    this.possibleMove = false
+
+    if (piece) this.piece.square = this
+  }
+
+  isEmpty() {
+    return !this.piece
+  }
+
+  removePiece() {
+    this.piece.square = null
+    this.piece = null
+  }
+
+  putPiece(piece) {
+    piece.square = this
+    this.piece = piece
+  }
+
+  getXY() {
+    const index = this.index
+    return {x: index % 8, y: Math.floor(index / 8)}
+  }
+
+  select() {
+    if (this.isEmpty()) return false
+    return this.piece.select()
+  }
+
+  isEnPassant() {
+    const board = this.board
+    const {x, y} = this.getXY()
+    return board.enPassant && x === board.enPassant.x && y === board.enPassant.y
+  }
+}
--- client/src/reducers/gameReducer.js
+++ client/src/reducers/gameReducer.js
@@ -1,51 +1,65 @@
-import {INITIAL_PIECES} from '../consts/game';
-import {TIMER_VALUES} from "../consts/game";
-import axios from 'axios';
+import Board from "../models/Board"
 
+const SELECT_PIECE = "SELECT_PIECE"
+const UNSELECT_PIECE = "UNSELECT_PIECE"
+const MOVE_PIECE = "MOVE_PIECE"
 
-const INIT = "INIT";
-const API_URL = 'http://localhost:8000';
-
-export const NEW_GAME = {
-  pieces: INITIAL_PIECES,
-  castledBlack: false,
-  castledWhite: false,
-};
-
-export const gameReducer = (state, action) => {
-
-  switch(action.type) {
-  case "INIT":
-    console.log("HIT INIT");
-    return {...NEW_GAME};
-  case "ADD":
-    return {
-      ...state,
-      pieces: {...state.pieces,
-		[action.payload.to]: action.payload.piece}};
-  case "MOVE":
-    return {
-      ...state,
-      pieces: {...state.pieces,
-	       [action.payload.from]: undefined,
-	       [action.payload.to]: action.payload.piece}};
-  default:
-    return null;
-  }
-};
-
-export const initGame = (timeMode, authUser, accessToken) => async (dispatch) => {
-  const resp = await axios({
-    method: 'post',
-    url: `${API_URL}/game/init_game`,
-    headers: {
-      'content-type': 'application/json',
-      'Authorization': `token ${accessToken}`},
-    data: {
-      user: authUser,
-      white_player: 2,
-      black_player: 3,
-      timer: TIMER_VALUES[timeMode]
-    }
-  });
+const initialState = {
+  board: new Board(),
 }
+
+export default function gameReducer(state = initialState, action) {
+  const {index} = action.payload || {}
+  const targetSquare = index != null && state.board.squares[index]
+  console.log(state.board.toFen())
+  switch (action.type) {
+    case SELECT_PIECE:
+      if (targetSquare.select()) return {...state}
+      return state
+    case UNSELECT_PIECE:
+      state.board.unselectPiece()
+      return {...state}
+    case MOVE_PIECE:
+      state.board.selectedPiece.move(index) ||
+        (!targetSquare.isEmpty() &&
+          !targetSquare.piece.selected &&
+          targetSquare.select()) ||
+        state.board.unselectPiece()
+      return {...state}
+    default:
+      return state
+  }
+}
+
+export const selectPiece = (index) => {
+  return {type: SELECT_PIECE, payload: {index}}
+}
+
+export const unselectPiece = () => {
+  return {type: UNSELECT_PIECE}
+}
+
+export const movePiece = (index) => {
+  return {type: MOVE_PIECE, payload: {index}}
+}
+
+// export const initGame = (timeMode, authUser, accessToken) => async (dispatch) => {
+//   console.log(timeMode);
+//   console.log(authUser);
+//   console.log(accessToken);
+//   console.log(TIMER_VALUES[timeMode]);
+//   const resp = await axios({
+//     method: 'post',
+//     url: `${API_URL}/game/init_game`,
+//     headers: {
+//       'content-type': 'application/json',
+//       'Authorization': `token ${accessToken}`},
+//     data: {
+//       user: authUser,
+//       white_player: 2,
+//       black_player: 3,
+//       timer: TIMER_VALUES[timeMode]
+//     }
+//   });
+//   console.log(resp.data);
+//   console.log(dispatch);
