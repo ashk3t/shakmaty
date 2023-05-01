fc2f75644855ff27bb9d277c5668a7b00ced835c
shk3t <daniildudov@mail.ru>
2023-05-01 20:51:21 +0300

    Login + Register + Settings

--- client/src/services/AuthService.js
+++ client/src/services/AuthService.js
@@ -2,18 +2,18 @@ import {publicConfig, withCredentialsConfig} from "../http"
 
 export default class AuthService {
   static async register(credentials) {
-    const response = await publicConfig.post("/game/register", credentials)
+    const response = await publicConfig.post("/api/register", credentials)
     return response.data
   }
   static async login(credentials) {
-    const response = await publicConfig.post("/game/login", credentials)
+    const response = await publicConfig.post("/api/login", credentials)
     return response.data
   }
   static async logout() {
-    await publicConfig.post("game/logout")
+    await publicConfig.post("api/logout")
   }
   static async refreshTokens() {
-    const response = await withCredentialsConfig.post("/game/tokens/refresh")
+    const response = await withCredentialsConfig.post("/api/tokens/refresh")
     return response.data
   }
 }
