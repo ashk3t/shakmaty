3c8ab72f69803f8eb366759271e0d9ef8e1c140c
Matvey Chukhno <mr.chucno@yandex.ru>
2023-05-01 20:39:37 +0300

    Backend changes

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,25 +1,6 @@
 import React from "react"
 import classes from "../styles/Header.module.css"
-import mister from "../assets/mainPageImgs/mister.png"
 
 export default function Header() {
-    return (
-        <div>
-            <div className={classes.panel}>
-                <div className={classes.imgWrap}>
-                    <img className={classes.avatar} src={mister}/>
-                </div>
-                <div className={classes.nickname}>Nick</div>
-                <div className={classes.status}>В сети</div>
-                    <nav>
-                        <ul>
-                            <li><a href='./main'>Главная станица</a></li>
-                            <li><a href="./statistic">Личный кабинет</a></li>
-                            <li><a href="#">Настройки</a></li>
-                        </ul>
-                    </nav>
-            </div>  
-        </div>
-            
-    )
+  return <header className={classes.example}>This is Header</header>
 }
--- client/src/consts.js
+++ client/src/consts.js
@@ -1,12 +1,9 @@
-import MainPageGame from "./components/pages/MainPageGame"
-import StatisticPage from "./components/pages/Statistic"
+import MainPage from "./components/pages/MainPage"
 
 // Pathes
 export const MAIN_PATH = "/main"
-export const STATISTIC_PATH = "/statistic"
 
 // Routes
 export const routes = [
-  { path: MAIN_PATH, Component: MainPageGame},
-  { path: STATISTIC_PATH, Component: StatisticPage}
+  { path: MAIN_PATH, Component: MainPage}
 ]
